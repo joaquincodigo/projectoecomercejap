af4736d58b50dd9264c980df79d1a4c9576848e8
RipNephils <59967020+RipNephils@users.noreply.github.com>
2022-08-20 12:50:04 -0300

    index y login completos

--- js/login.js
+++ js/login.js
@@ -1,30 +1,32 @@
+function showElement(element) {
+  element.setAttribute("style", "display: block !important")
+}
+
+function hideElement(element) {
+  element.setAttribute("style", "display: none !important")
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  
-  
-  
-    let loginFormElement = document.getElementById("login-form");
-    let emailInputElement = document.getElementById("email-input")
-    let passwordInputElement = document.getElementById("password-input")
-    let wrongInputAlertElement = document.getElementById("wrong-input-alert")
+  console.log("> DOM Loaded");
+  let loginPageContainer = document.getElementById('login-page-container');
+  let landPageContainer = document.getElementById('landpage-container');
+  let loginFormElement = document.getElementById('login-form');
+  // let emailInputElement = document.getElementById("email-input");
+  // let passwordInputElement = document.getElementById("password-input");
+  // let wrongInputAlertElement = document.getElementById("wrong-input-alert");
 
-    loginFormElement.addEventListener("submit", (event) => {
+  loginFormElement.addEventListener("submit", (event) => {
     event.preventDefault();
-      if (passwordInputElement.value.length <= 5) {
-      
-
-
-
-
-        //TODO ALERT SYLE
-      wrongInputAlertElement.innerHTML = "Hello"
-      wrongInputAlertElement.style.display = "block";
-      
-      
-    };
+    console.log("> Login form submitted");
+  
+    hideElement(loginPageContainer)
+    console.log("> Login page hidden");
+    
+    showElement(landPageContainer)
+    console.log("> Landing page displayed");
+    
   });
-
-    
-    
-    
-    
 });
+
+
+
