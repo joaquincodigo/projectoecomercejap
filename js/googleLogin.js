39c799b2cb84cd7efbaf9e7c411a87a808ecfb6a
RipNephils <59967020+RipNephils@users.noreply.github.com>
2022-08-21 16:58:00 -0300

    trying to decode the token again

--- js/googleLogin.js
+++ js/googleLogin.js
@@ -2,8 +2,8 @@
 //   console.log("Encoded JWT ID token: " + response.credential);
 // }
 
-function parseJwt (token) {
-  var base64Url = token.split('.')[1];
+function parseJwt (reponse) {
+  var base64Url = reponse.credential.split('.')[1];
   var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
   var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
