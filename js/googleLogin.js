3bc45fda1c41c5f1ebdeeccdfd2bc5e8f0eb5b10
RipNephils <59967020+RipNephils@users.noreply.github.com>
2022-08-21 17:17:03 -0300

    3rd try of decoding the token

--- js/googleLogin.js
+++ js/googleLogin.js
@@ -1,23 +1,21 @@
+import jwt_decode from 'jwt-decode'
+
+function handler(reponse) {
+  let token = reponse.credential
+  let decoded = jwt_decode(token)
+  console.log(decoded)
+  return decoded
+}
+
 // function handleCredentialResponse(response) {
 //   console.log("Encoded JWT ID token: " + response.credential);
 // }
-
-function parseJwt (reponse) {
-  var base64Url = reponse.credential.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-
-  console.log(JSON.parse(jsonPayload))
-  return JSON.parse(jsonPayload);
-};
 
 window.onload = function () {
   google.accounts.id.initialize({
     client_id:
       "712828991122-5opoavq9gvvuokquo7cue7radto24igg.apps.googleusercontent.com",
-    callback: parseJwt,
+    callback: handler,
   });
   google.accounts.id.renderButton(
     document.getElementById("googleButtonDiv"),
