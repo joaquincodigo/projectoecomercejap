4e4df706365a930033daf57342ca1d0888dbe9e6
Rip Nephils <RipNephils@gmail.com>
2022-08-26 17:29:03 -0300

    Added username to the navbar

--- js/login.js
+++ js/login.js
@@ -3,13 +3,13 @@ function isInputEmpty(inputField) {
   return inputField.value == "";
 }
 
-function showElement(element) {
-  element.setAttribute("style", "display: block !important");
-}
+// function showElement(element) {
+//   element.setAttribute("style", "display: block !important");
+// }
 
-function hideElement(element) {
-  element.setAttribute("style", "display: none !important");
-}
+// function hideElement(element) {
+//   element.setAttribute("style", "display: none !important");
+// }
 
 function showInvalidInputMessage(messageString, inputElement) {
   
@@ -31,8 +31,8 @@ function showInvalidInputMessage(messageString, inputElement) {
 document.addEventListener("DOMContentLoaded", () => {
   
   // Defining HTML elements
-  let loginPageContainer = document.getElementById("login-page-container");
-  let landPageContainer = document.getElementById("landpage-container");
+  // let loginPageContainer = document.getElementById("login-page-container");
+  // let landPageContainer = document.getElementById("landpage-container");
   let loginFormElement = document.getElementById("login-form");
   let userInputElement = document.getElementById("user-input");
   let passwordInputElement = document.getElementById("password-input");
@@ -48,14 +48,16 @@ document.addEventListener("DOMContentLoaded", () => {
         "El usuario ingresado no es válido.",
         userInputElement
       );
-    } else if (isInputEmpty(passwordInputElement)) {
+    }
+    else if (isInputEmpty(passwordInputElement)) {
       showInvalidInputMessage(
         "La contraseña ingresada no es válida.",
         passwordInputElement
       );
-    } else {
-      hideElement(loginPageContainer);
-      showElement(landPageContainer);
+    }
+    else {
+      localStorage.setItem("username", userInputElement.value)
+      window.location.href = "landpage.html"
     }
   });
 });
