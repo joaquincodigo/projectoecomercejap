4cc7f6d2b1f762b6ba1729f4e389c94eef2fdf95
Rip Nephils <RipNephils@gmail.com>
2022-09-06 17:53:30 -0300

    designing product info page

--- js/product-info.js
+++ js/product-info.js
@@ -1,11 +1,17 @@
 //// DECLARING VARIABLES ////
 const PRODUCT_ID = localStorage.getItem("productId");
-const mainElement = document.getElementsByName("main")[0];
+const mainElement = document.getElementsByTagName("main")[0];
 
 //// DEFINING FUNCTIONS ////
-function insertProduct(product) {
-  let productTitle = (document.createElement("p").innerHTML = product.id);
-  mainElement.appendChild(productTitle);
+function insertProductElements(product) {
+  let title = (document.createElement("p").innerHTML = product.name);
+  let images = (document.createElement("img").src = product.images[0]);
+  let description = (document.createElement("p").innerHTML =
+    product.description);
+  //   let description = (document.createElement("p").innerHTML = product.description);
+  //     let description = (document.createElement("p").innerHTML = product.description);
+
+  mainElement.innerHTML = image;
 }
 
 //// ADDING EVENT LISTENERS ////
@@ -15,6 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
   insertNavbar();
   getJSONData(PRODUCT_INFO_URL + PRODUCT_ID + EXT_TYPE).then((response) => {
     let productData = response.data;
-    insertProduct(productData);
+    insertProductElements(productData);
   });
 });
