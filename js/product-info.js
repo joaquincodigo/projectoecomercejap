7bcd0ed59452a09c03c9c15b9a25330dae6358f8
Rip Nephils <RipNephils@gmail.com>
2022-09-15 13:52:50 -0300

    first draft of comment publishing

--- js/product-info.js
+++ js/product-info.js
@@ -5,21 +5,20 @@ const PRODUCT_ID = localStorage.getItem("productId");
 const PRODUCT_URL = PRODUCT_INFO_URL + PRODUCT_ID + EXT_TYPE;
 const COMMENTS_URL = PRODUCT_INFO_COMMENTS_URL + PRODUCT_ID + EXT_TYPE;
 
-const PRODUCT_NAME_ELEMENT = document.getElementById("product-name");
-const PRODUCT_IMAGE_ELEMENT = document.getElementById("product-image");
-const PRODUCT_CURRENCY_ELEMENT = document.getElementById("product-currency");
-const PRODUCT_COST_ELEMENT = document.getElementById("product-cost");
-const PRODUCT_DESCRIPTION_ELEMENT = document.getElementById(
-  "product-description"
-);
-const COMMENT_BUTTON_ELEMENT = document.getElementById("comment-button");
-const COMMENT_INPUT_ELEMENT = document.getElementById("comment-input");
-const RELATED_PRODUCTS_CONTAINER_ELEMENT = document.getElementById(
+const PRODUCT_NAME_ELEM = document.getElementById("product-name");
+const PRODUCT_IMAGE_ELEM = document.getElementById("product-image");
+const PRODUCT_CURRENCY_ELEM = document.getElementById("product-currency");
+const PRODUCT_COST_ELEM = document.getElementById("product-cost");
+const PRODUCT_DESCRIPTION_ELEM = document.getElementById("product-description");
+const COMMENT_INPUT_BUTTON_ELEM = document.getElementById("comment-button");
+const COMMENT_INPUT_ELEM = document.getElementById("comment-input");
+const RELATED_PRODUCTS_CONTAINER_ELEM = document.getElementById(
   "related-products-container"
 );
-const COMMENT_INPUT_STARS =
+const COMMENT_INPUT_STARS_ELEMS =
   document.getElementsByClassName("comment-input-star");
-const COMMENT_CONTAINER_ELEMENT = document.getElementById("comments-container");
+const COMMENTS_CONTAINER_ELEM = document.getElementById("comments-container");
+const COMMENT_INPUT_FORM_ELEM = document.getElementById("comment-input");
 
 //DEFINING FUNCTIONS
 function insertBreadcrumsBar() {
@@ -27,11 +26,11 @@ function insertBreadcrumsBar() {
 }
 
 function insertProductData() {
-  PRODUCT_NAME_ELEMENT.innerText = PRODUCT_OBJECT.name;
-  PRODUCT_IMAGE_ELEMENT.src = PRODUCT_OBJECT.images[0];
-  PRODUCT_DESCRIPTION_ELEMENT.innerText = PRODUCT_OBJECT.description;
-  PRODUCT_CURRENCY_ELEMENT.innerText = PRODUCT_OBJECT.currency;
-  PRODUCT_COST_ELEMENT.innerText = PRODUCT_OBJECT.cost;
+  PRODUCT_NAME_ELEM.innerText = PRODUCT_OBJECT.name;
+  PRODUCT_IMAGE_ELEM.src = PRODUCT_OBJECT.images[0];
+  PRODUCT_DESCRIPTION_ELEM.innerText = PRODUCT_OBJECT.description;
+  PRODUCT_CURRENCY_ELEM.innerText = PRODUCT_OBJECT.currency;
+  PRODUCT_COST_ELEM.innerText = PRODUCT_OBJECT.cost;
 }
 
 function insertRelatedProducts() {
@@ -52,76 +51,100 @@ function insertRelatedProducts() {
     containerElement.appendChild(nameElement);
     containerElement.appendChild(imageElement);
 
-    RELATED_PRODUCTS_CONTAINER_ELEMENT.appendChild(containerElement);
+    RELATED_PRODUCTS_CONTAINER_ELEM.appendChild(containerElement);
   }
 }
 
-function fetchAndInsertComments() {
+function insertAPIComments() {
   // Modifying the DOM by using the document object methods
   fetch(COMMENTS_URL)
     .then((response) => response.json())
     .then((commentsObjectsArray) => {
       for (const commentObject of commentsObjectsArray) {
-        insertComment(commentObject.description, commentObject.score, commentObject.dateTime);
+        ({ description, score, dateTime } = commentObject);
+        insertComment(description, score, dateTime);
       }
     });
 }
 
 function insertComment(commentText, commmentScore, commentaDate) {
-  let commentTextElement = document.createElement("p");
-        let commentDateElement = document.createElement("p");
-        let commentScoreElement = document.createElement("div");
-        let commentSeparatorElement = document.createElement("hr");
+  let commentTextElem = document.createElement("p");
+  let commentDateElem = document.createElement("p");
+  let commentScoreElem = document.createElement("div");
+  let commentSeparatorElem = document.createElement("hr");
 
-        commentTextElement.classList.add("comment-text");
-        commentTextElement.innerText = commentText;
+  commentTextElem.classList.add("comment-text");
+  commentTextElem.innerText = commentText;
 
-        commentDateElement.classList.add("comment-date");
-        commentDateElement.innerText = commentaDate;
+  commentDateElem.classList.add("comment-date");
+  commentDateElem.innerText = commentaDate;
 
-        commentScoreElement.classList.add = "comment-score";
-        for (let i = 1; i <= 5; i++) {
-          let starIcon = document.createElement("span");
-          starIcon.classList.add("fa");
-          starIcon.classList.add("fa-star");
-          starIcon.classList.add("comment-score");
-          if (i <= commmentScore) {
-            starIcon.classList.add("checked");
-          }
-          commentScoreElement.appendChild(starIcon);
-        }
+  commentScoreElem.classList.add = "comment-score";
+  for (let i = 1; i <= 5; i++) {
+    let starIcon = document.createElement("span");
+    starIcon.classList.add("fa");
+    starIcon.classList.add("fa-star");
+    starIcon.classList.add("comment-score");
+    if (i <= commmentScore) {
+      starIcon.classList.add("checked");
+    }
+    commentScoreElem.appendChild(starIcon);
+  }
 
-        COMMENT_CONTAINER_ELEMENT.appendChild(commentScoreElement);
-        COMMENT_CONTAINER_ELEMENT.appendChild(commentDateElement);
-        COMMENT_CONTAINER_ELEMENT.appendChild(commentTextElement);
-        COMMENT_CONTAINER_ELEMENT.appendChild(commentSeparatorElement)
+  COMMENTS_CONTAINER_ELEM.appendChild(commentScoreElem);
+  COMMENTS_CONTAINER_ELEM.appendChild(commentDateElem);
+  COMMENTS_CONTAINER_ELEM.appendChild(commentTextElem);
+  COMMENTS_CONTAINER_ELEM.appendChild(commentSeparatorElem);
 }
 
+// function sortNodes() {
+//   let commen = document.getElementById("mylist");
+//   var items = list.childNodes;
+//   var itemsArr = [];
+//   for (var i in items) {
+//     if (items[i].nodeType == 1) {
+//       // get rid of the whitespace text nodes
+//       itemsArr.push(items[i]);
+//     }
+//   }
+
+//   itemsArr.sort(function (a, b) {
+//     return a.innerHTML == b.innerHTML ? 0 : a.innerHTML > b.innerHTML ? 1 : -1;
+//   });
+
+//   for (i = 0; i < itemsArr.length; ++i) {
+//     list.appendChild(itemsArr[i]);
+//   }
+// }
 
 function addCommentInputStarsHoverAnimation() {
-  
   // Adding event listener to each star
-  for (let i = 0; i < COMMENT_INPUT_STARS.length; i++) {
-    COMMENT_INPUT_STARS[i].addEventListener("mouseover", () => {
-
+  for (let i = 0; i < COMMENT_INPUT_STARS_ELEMS.length; i++) {
+    COMMENT_INPUT_STARS_ELEMS[i].addEventListener("mouseover", () => {
       // Uncheking all the stars (reset)
-      for (const star of COMMENT_INPUT_STARS) {
+      for (const star of COMMENT_INPUT_STARS_ELEMS) {
         star.classList.remove("checked");
       }
 
       // Checking all the stars up to the selected one
       for (let j = 0; j <= i; j++) {
-        COMMENT_INPUT_STARS[j].classList.add("checked");
+        COMMENT_INPUT_STARS_ELEMS[j].classList.add("checked");
       }
     });
   }
 }
 
-COMMENT_BUTTON_ELEMENT.addEventListener("click", (event) => {
+// ADDING EVENT LISTENERS
+COMMENT_INPUT_BUTTON_ELEM.addEventListener("click", (event) => {
   event.preventDefault();
-  let commentInputStarsArray = Array.from(COMMENT_INPUT_STARS)  
-  let commentInputStarsChecked = commentInputStarsArray.filter((star) => star.classList.contains("checked") )
-  alert(COMMENT_INPUT_ELEMENT.value + " " + commentInputStarsChecked.length);
+  let commentInputStarsArray = Array.from(COMMENT_INPUT_STARS_ELEMS);
+  let commentInputStarsChecked = commentInputStarsArray.filter((star) =>
+    star.classList.contains("checked")
+  );
+  let commentText = COMMENT_INPUT_ELEM.value
+  let commentScore = commentInputStarsChecked.length
+  let commentDate = new Date(Date.now()).toLocaleDateString();
+  insertComment(commentText, commentScore, commentDate)
 });
 
 // ON DOM LOADED
@@ -132,7 +155,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // insertBreadcrumsBar();
     insertProductData();
     insertRelatedProducts();
-    fetchAndInsertComments();
+    insertAPIComments();
     addCommentInputStarsHoverAnimation();
   });
 });
