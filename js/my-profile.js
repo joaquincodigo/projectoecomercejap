3b5e6baa1c1fcbcc968975c3392e42f7ca518bf1
Rip Nephils <RipNephils@gmail.com>
2022-11-01 17:39:32 -0300

    added dropzone to the profile

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,8 +1,17 @@
-// VARIALES
-let PROFILE_FORM_ELEM = document.getElementById("profile-form");
-let USER_NOT_LOGGED_ALERT_ELEM = document.getElementById(
+// VARIABLES
+const PROFILE_FORM_ELEM = document.getElementById("profile-form");
+const NAME_INPUT_ELEM = document.getElementById("name-input");
+const SECOND_NAME_INPUT_ELEM = document.getElementById("second-name-input");
+const SURNAME_INPUT_ELEM = document.getElementById("surname-input");
+const SECOND_SURNAME_INPUT_ELEM = document.getElementById(
+  "second-surname-input"
+);
+const EMAIL_INPUT_ELEM = document.getElementById("email-input");
+const PHONE_INPUT_ELEM = document.getElementById("phone-input");
+const USER_NOT_LOGGED_ALERT_ELEM = document.getElementById(
   "user-not-logged-alert"
 );
+const SUBMIT_BUTTON_ELEM = document.getElementById("submit-button");
 
 // FUNCTIONS
 function isCurrentUserLoggedIn() {
@@ -18,6 +27,29 @@ function displayElement(element) {
   element.classList.add("d-block");
 }
 
+function saveProfileDataToLocalStorage() {
+  let profileData = {
+    name: NAME_INPUT_ELEM.value,
+    secondName: SECOND_NAME_INPUT_ELEM.value,
+    surname: SURNAME_INPUT_ELEM.value,
+    secondSurname: SECOND_SURNAME_INPUT_ELEM.value,
+    email: EMAIL_INPUT_ELEM.value,
+    phone: PHONE_INPUT_ELEM.value,
+  };
+  localStorage.setItem("profileData", JSON.stringify(profileData));
+}
+
+function setupImageDropzoneElement() {
+  let dzoptions = {
+    url: "/",
+    autoQueue: false,
+  };
+  let myDropzone = new Dropzone("div#file-upload", dzoptions);
+}
+
+// EVENT LISTENERS
+SUBMIT_BUTTON_ELEM.addEventListener("", () => {});
+
 // ON DOM LOADED
 document.addEventListener("DOMContentLoaded", () => {
   insertNavbar();
@@ -26,10 +58,12 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     displayElement(USER_NOT_LOGGED_ALERT_ELEM);
   }
+  setupImageDropzoneElement();
 });
 
 // TO DO:
-// > Solamente se podrá ingresar al perfil si el user esta loggeado.
+
+// DONE > Solamente se podrá ingresar al perfil si el user esta loggeado.
 // > Al ingresar por primera vez, todos los campos deben estar vacios excepto el email que debe ser el del login.
 // > Al darle al botón, luego de validar, guardar en el local storage.
 // > La siguiente vez que se ingrese ya deben estar todos los datos cargadores desde el LS
